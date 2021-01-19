4203de4987e821d0d265aec92cfe44eb7b227422
Your Name <you@example.com>
2021-01-18 22:55:13 -0600

    needed to change come values

--- week-2/ejs-views/fulfs-assignment-2.4.js
+++ week-2/ejs-views/fulfs-assignment-2.4.js
@@ -5,16 +5,16 @@ var app = express(); //add for the express app
 
 var path = require("path");
 
-app.set("views", path.resolve(__dirname, "views")); // Tell Express the views are in the 'views' directory
+app.set("ejs-views", path.resolve(__dirname, "ejs-views")); // Tell Express the views are in the 'ejs-views' directory
 
-app.set("view engine", "ejs"); // Tell Express to use the EJS view engine
+app.set("ejs-views engine", "ejs"); // Tell Express to use the EJS view engine
 
 app.get("/", function(request, response) {
 
-    response.render("index", {
+    response.render("index.ejs", {
 
-        firstName: "Perry"
-        lastName: "Fulfs\n"
+        firstName: "Perry",
+        lastName: "Fulfs\n",
         address: "Omaha, NE 68107"
 
    });
