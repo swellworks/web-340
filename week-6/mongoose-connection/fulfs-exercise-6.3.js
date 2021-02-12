23e6794f8133d8d0df4fc5fc20e3e23400f1cb6a
Perry Fulfs <perry@swellworks.com>
2021-02-11 20:37:32 -0600

    commit 6.3 - getting errors

--- week-6/mongoose-connection/fulfs-exercise-6.3.js
+++ week-6/mongoose-connection/fulfs-exercise-6.3.js
@@ -4,4 +4,20 @@ var http = require('http');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 
-var mongoDB = ''
+var mongoDB = 'mongodb+srv://swellworks:admin@cluster0.nclbl.mongodb.net/fulfs-ems?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, {
+    // useMongoClient: true
+});
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error: ' ));
+db.once('open', function(){
+    console.log('Application connected to mLab');
+});
+
+var app = express();
+app.use(logger('dev'));
+
+http.createServer(app).listen(5000, function (){
+    console.log('Application started and listening on port 5000');
+});
