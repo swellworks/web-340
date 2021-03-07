ba9e2cc5d8ae1edb4afb7feea136a6d0e2895d5d
Perry Fulfs <perry@swellworks.com>
2021-03-06 23:11:28 -0600

    commit app, list

--- ems/app.js
+++ ems/app.js
@@ -67,20 +67,24 @@ app.get("/", function(req, res) {
     });
 });
 
-app.post("/process", function(req, res) {
-    console.log(req.body.txtName);
-    res.redirect("/");
-
-});
-
-
 // list page
-app.get('/list', function(req, res) {
-    res.render('list.ejs', {
-        title: "List Page",
-        message: "List Page"
+app.get("/list", function(req, res) {
+    Employee.find({}, function(error, employees) {
+       if (error) throw error;
+       res.render("list", {
+           title: "Employee List",
+           employees: employees
+       });
     });
 });
+
+
+// app.get('/list', function(req, res) {
+//     res.render('list.ejs', {
+//         title: "List Page",
+//         message: "List Page"
+//     });
+// });
 
 // new page
 app.get('/new', function(req, res) {
@@ -98,7 +102,32 @@ app.get('/view', function(req, res) {
     });
 });
 
+// post
+app.post("/process", function(req, res) {
+    // console.log(request.body.txtName);
+    if (!req.body.txtName) {
+        res.status(400).send("Entries must have a name");
+        return;
+    }
+
+    // get the request's form data
+    var employeeName = request.body.txtName;
+    console.log(employeeName);
+ 
+    // create a fruit model
+    var employee = new Employee({
+        name: employeeName
+    });
+ 
+    // save
+    fruit.save(function (error) {
+        if (error) throw error;
+        console.log(employeeName + " saved successfully!");
+    });
+    response.redirect("/");
+ });
+
 // create/start Node server
-http.createServer(app).listen(3004, function() {
-    console.log("Application started on port 3004!");
+http.createServer(app).listen(3005, function() {
+    console.log("Application started on port 3005!");
 });
